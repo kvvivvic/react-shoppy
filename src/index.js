5c6e1e63f36cfe6dfd3a7c0eea6a3fd5ce2c00dc
kvvivvic <rnjs9337@gmail.com>
2023-01-20 16:07:49 +0900

    header 제작

--- src/index.js
+++ src/index.js
@@ -1,13 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import NotFound from "./pages/NotFound";
+import Home from "./pages/Home";
+import AllProducts from "./pages/AllProducts";
+import MyCart from "./pages/MyCart";
+import NewProducts from "./pages/NewProducts";
+import ProductDetail from "./pages/ProductDetail";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <NotFound />,
+    children: [
+      { index: true, path: "/", element: <Home /> },
+      { path: "/products", element: <AllProducts /> },
+      { path: "/products/new", element: <NewProducts /> },
+      { path: "/products/:id", element: <ProductDetail /> },
+      { path: "/cart", element: <MyCart /> },
+    ],
+  },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
