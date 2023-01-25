c769528d082c4d3accfcef4e4c425b5478e9efba
kvvivvic <rnjs9337@gmail.com>
2023-01-25 16:37:28 +0900

    product 페이지 완성

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import { Outlet } from 'react-router-dom';
-import { AuthContextProvider } from './components/context/AuthContext';
-import Navbar from './components/Navbar';
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import { Outlet } from "react-router-dom";
+import { AuthContextProvider } from "./components/context/AuthContext";
+import Navbar from "./components/Navbar";
 
 function App() {
+  const queryClient = new QueryClient();
   return (
-    <AuthContextProvider>
-      <Navbar />
-      <Outlet />
-    </AuthContextProvider>
+    <QueryClientProvider client={queryClient}>
+      <AuthContextProvider>
+        <Navbar />
+        <Outlet />
+      </AuthContextProvider>
+    </QueryClientProvider>
   );
 }
 
